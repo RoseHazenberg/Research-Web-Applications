a5d2bba91bb9180a6e50660ba0858b237b244880
akastia <n.akastia@gmail.com>
2021-12-07 21:57:05 +0100

    Changed the atom.html so it accepts the template.html

--- src/main/java/nl/bioinf/group5/servlets/AtomServlet.java
+++ src/main/java/nl/bioinf/group5/servlets/AtomServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 
-@WebServlet(name = "AtomServlet", urlPatterns = "/atom", loadOnStartup = 1)
+@WebServlet(name = "AtomServlet", urlPatterns = {"/atom"}, loadOnStartup = 1)
 public class AtomServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
