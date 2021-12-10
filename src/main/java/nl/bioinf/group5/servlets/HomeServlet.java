7bbe6b72a210453ad844877678efb0b0d238f092
Rose Hazenberg <rosehazenberg@gmail.com>
2021-12-10 15:33:55 +0100

    Changed the html and servlet of the home page

--- src/main/java/nl/bioinf/group5/servlets/HomeServlet.java
+++ src/main/java/nl/bioinf/group5/servlets/HomeServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 @WebServlet(name = "HomeServlet", urlPatterns = "/home", loadOnStartup = 1)
 public class HomeServlet extends HttpServlet {
