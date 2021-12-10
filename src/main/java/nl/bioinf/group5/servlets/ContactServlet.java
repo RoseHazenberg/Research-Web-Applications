4a4b88c203d8635924be4487d1c85990dc4fd9e8
Rose Hazenberg <rosehazenberg@gmail.com>
2021-12-10 15:25:38 +0100

    Added the content of the contact page

--- src/main/java/nl/bioinf/group5/servlets/ContactServlet.java
+++ src/main/java/nl/bioinf/group5/servlets/ContactServlet.java
@@ -1,2 +1,40 @@
-package nl.bioinf.group5.servlets;public class ContactServlet {
+package nl.bioinf.group5.servlets;
+import nl.bioinf.group5.config.WebConfig;
+import org.thymeleaf.context.WebContext;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ContactServlet", urlPatterns = "/contact", loadOnStartup = 1)
+public class ContactServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Initializing Thymeleaf template engine");
+        final ServletContext servletContext = this.getServletContext();
+        WebConfig.createTemplateEngine(servletContext);
+    }
+    private static final long serialVersionUID = 1L;
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        process(request, response);
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        process(request, response);
+    }
+    public void process(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        //this step is optional; standard settings also suffice
+        WebConfig.configureResponse(response);
+        WebContext ctx = new WebContext(
+                request,
+                response,
+                request.getServletContext(),
+                request.getLocale());
+        WebConfig.createTemplateEngine(getServletContext()).
+                process("contact", ctx, response.getWriter());
+
+    }
 }
